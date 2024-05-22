12f695d2f88e1885f939ccf9c84d9371a31e99e3
Richard Holyoak <rholyoak@gmail.com>
2024-05-22 20:30:35 +0100

    Audio

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,10 +1,11 @@
 class_name Coin extends Area2D
 
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
 var value = 1
 
 signal coin_collected(value)
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("+1 coin!")
 	coin_collected.emit(value)
-	queue_free()
+	animation_player.play("pickup")
